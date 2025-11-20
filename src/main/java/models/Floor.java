56fe0557a71c47615bca98008461ec27e3581ebb
Sylwia Wolańska <s30753@pjwstk.edu.pl>
2025-11-20 15:22:58 +0100

    [1B] floor-update
    created Floor class with its elements

---
+++ src/main/java/models/Floor.java
@@ -0,0 +1,28 @@
+package models;
+
+import annotations.Min;
+import annotations.NotNull;
+
+public class Floor {
+    @NotNull
+    @Min(value = 0)
+    private int number;
+    @NotNull
+    @Min(value = 1)
+    private int amountOfRooms;
+
+    public Floor() {}
+
+    public Floor(int number, int amountOfRooms) {
+        this.number = number;
+        this.amountOfRooms = amountOfRooms;
+    }
+
+    public int getNumber() { return number; }
+
+    public void setNumber(int number) { this.number = number; }
+
+    public int getAmountOfRooms() { return amountOfRooms; }
+
+    public void setAmountOfRooms(int amountOfRooms) { this.amountOfRooms = amountOfRooms; }
+}
