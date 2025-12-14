78805f4291af0ea3ba70702d1612366ac2956daa
0hosho <s30407@pjwstk.edu.pl>
2025-12-15 00:39:53 +0200

    SCRUM-25 Add OperationRoom-Operation-Doctor-Nurse-Patient associations with tests

--- src/main/java/models/Operation.java
+++ src/main/java/models/Operation.java
@@ -1,10 +1,14 @@
 package models;
 
+import annotations.NotEmpty;
 import annotations.NotNull;
 import annotations.ValidDate;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
@@ -26,6 +30,20 @@ public class Operation {
     @ValidDate(value = ValidDate.Mode.FUTURE)
     private Operation.Status status;
 
+    @NotNull
+    private OperationRoom operationRoom;
+
+    @NotNull
+    private Patient patient;
+
+    @NotNull
+    private Doctor doctor;
+
+    @NotNull
+    @NotEmpty
+    @JsonDeserialize(as = ArrayList.class)
+    private List<Nurse> nurses = new ArrayList<>();
+
     public Operation() {}
     public Operation(Timestamp startTime) {
         this.startTime = startTime;
@@ -35,10 +53,27 @@ public class Operation {
     public Operation.Status getStatus() { return status; }
     public Timestamp getStartTime() { return startTime; }
     public Timestamp getEndTime() { return endTime; }
+    public OperationRoom getOperationRoom() { return operationRoom; }
+    public Patient getPatient() { return patient; }
+    public Doctor getDoctor() { return doctor; }
+    public List<Nurse> getNurses() { return nurses; }
 
     public void setStatus(Operation.Status status) { this.status = status; }
     public void setStartTime(Timestamp startTime) { this.startTime = startTime; }
     public void setEndTime(Timestamp endTime) { this.endTime = endTime; }
+    public void setOperationRoom(OperationRoom operationRoom) { this.operationRoom = operationRoom; }
+    public void setPatient(Patient patient) { this.patient = patient; }
+
+    public void setDoctor(Doctor doctor) {
+        if (doctor == null) throw new IllegalArgumentException("doctor cannot be null");
+        this.doctor = doctor;
+    }
+
+    public void setNurses(List<Nurse> nurses) {
+        if (nurses == null || nurses.isEmpty())
+            throw new IllegalArgumentException("nurses cannot be empty");
+        this.nurses = new ArrayList<>(nurses);
+    }
 
     /**
      *
